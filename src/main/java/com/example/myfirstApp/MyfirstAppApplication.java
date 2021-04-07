c7e41f2f970ed1343029e04dfadd986ce264eae6
kazumasa1111 <kazumasa_machida5298@icloud.com>
2021-04-07 14:15:04 +0900

    sample html
    MVCを利用したsamplecodeの記述

--- src/main/java/com/example/myfirstApp/MyfirstAppApplication.java
+++ src/main/java/com/example/myfirstApp/MyfirstAppApplication.java
@@ -2,17 +2,9 @@ package com.example.myfirstApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MyfirstAppApplication {
-
-	@RequestMapping("/")
-	public String home() {
-			return "hello";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyfirstAppApplication.class, args);
